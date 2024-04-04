8e2583c9bc7149034ffb250d2a39a2cfc96f7e40
Pascal Scholz <pascal.scholz@tu-dresden.de>
2024-04-04 14:08:54 +0200

    Add copy of default libpthread

--- uclibc/pthread.mk
+++ uclibc/pthread.mk
@@ -1,10 +1,10 @@
 PKGDIR ?= ..
 
 PTH_STOCK = libpthread
-PTH_W_TRACES = libpthread-perftrace
+PTH_LLFUTEX = libpthread-llfutex-port
 
 ifeq ($(CONFIG_UCLIBC_STOCK_TRACING),y)
-PTH_DIR ?= $(PKGDIR)/../uclibc/lib/$(PTH_W_TRACES)
+PTH_DIR ?= $(PKGDIR)/../uclibc/lib/$(PTH_LLFUTEX)
 else
 PTH_DIR ?= $(PKGDIR)/../uclibc/lib/$(PTH_STOCK)
 endif
