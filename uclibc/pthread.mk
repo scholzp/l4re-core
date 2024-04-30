c3f422126d96fe8eff766af73eb1b0b8962dd447
Pascal Scholz <pascal.scholz@tu-dresden.de>
2024-04-30 19:40:58 +0200

    uclibc: Rework pthreads menu and choices
    Because the current choice's names didn't reflect their actual purpose,
    some renaming was done. For consistency, this renaming is now reflected
    in the directory structure too.
    
    Added a new directory for a modified spinlock version.

--- uclibc/pthread.mk
+++ uclibc/pthread.mk
@@ -1,10 +1,27 @@
+# Makefile to include to set the right variables from config choices for pthreads.
 PKGDIR ?= ..
 
-PTH_STOCK = libpthread
-PTH_LLFUTEX = libpthread-llfutex-port
+# just define some names for the respective pthreads directory names.
+PTH_DEFAULT = libpthread
+PTH_SEMAPHORE = libpthread-semaphore
+PTH_SPINLOCK_MOD = libpthread-spinmod
+# This is the target for later use in the Makefile in the lib directory.
+# It only serves the purpose for building the correct version of pthreads.
+PTH_TARGET =$(PTH_DEFAULT)
 
-ifeq ($(CONFIG_UCLIBC_STOCK_TRACING),y)
-PTH_DIR ?= $(PKGDIR)/../uclibc/lib/$(PTH_LLFUTEX)
+# if a new path is to be added, sadly a new step must be added to this
+# if cascade.
+ifeq ($(CONFIG_UCLIBC_PTHREADS_DEFAULT),y)
+PTH_DIR ?= $(PKGDIR)/../uclibc/lib/$(PTH_DEFAULT)
+# default target for Makefile usage set a few line before
 else
-PTH_DIR ?= $(PKGDIR)/../uclibc/lib/$(PTH_STOCK)
-endif
+ifeq ($(CONFIG_UCLIBC_PTHREADS_SEMAPHORE),y)
+PTH_DIR ?= $(PKGDIR)/../uclibc/lib/$(PTH_SEMAPHORE)
+PTH_TARGET =$(PTH_SEMAPHORE)
+else
+ifeq ($(CONFIG_UCLIBC_PTHREADS_SPINLOCK_MODIFIED),y)
+PTH_DIR ?= $(PKGDIR)/../uclibc/lib/$(PTH_SPINLOCK_MOD)
+PTH_TARGET =$(PTH_SPINLOCK_MOD)
+endif # CONFIG_UCLIBC_PTHREADS_SEMAPHORE
+endif # CONFIG_UCLIBC_PTHREADS_SEMAPHORE
+endif # CONFIG_UCLIBC_PTHREADS_DEFAULT
