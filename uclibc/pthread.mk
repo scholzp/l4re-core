56b49820511aa62837de792c8a685c9b0e352a77
Pascal Scholz <pascal.scholz@tu-dresden.de>
2024-08-23 10:50:26 +0200

    First batch of Kconfig switch

---
+++ uclibc/pthread.mk
@@ -0,0 +1,10 @@
+PKGDIR ?= ..
+
+PTH_STOCK = libpthread
+PTH_W_TRACES = libpthread_futex
+
+ifeq ($(CONFIG_UCLIBC_STOCK_TRACING),y)
+PTH_DIR ?= $(PKGDIR)/../uclibc/lib/$(PTH_W_TRACES)
+else
+PTH_DIR ?= $(PKGDIR)/../uclibc/lib/$(PTH_STOCK)
+endif
